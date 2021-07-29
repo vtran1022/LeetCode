d8ba130858fa7d96732bc1684e0b627706b97518
Vy Tran <vtran1022@gmail.com>
2021-07-29 12:53:09 -0700

    Time: 148 ms (21.68%), Space: 45 MB (5.09%) - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -7,20 +7,15 @@ var removeDuplicates = function(nums) {
     if (tracker === 0) {
         return 0;
     }
-    
-    const checkNums = (i) => {
-      if (i === nums.length - 1) {
-          return;
-      }
-        if (nums[i] === nums[i + 1]) {
-            nums.push(nums.splice(i + 1, 1));
-            tracker--;
-            checkNums(i);
-        } else if (nums[i] < nums[i + 1]) {
-            checkNums(i + 1);
-        }
-    };
-    checkNums(0);
 
+    for (let i = 0; i < nums.length; i++) {
+        let j = i + 1;
+        if (nums[i] === nums[j]) {
+            nums.splice(j, 1);
+            tracker--;
+            return removeDuplicates(nums);
+        }
+    }
+    
     return tracker;
 };
