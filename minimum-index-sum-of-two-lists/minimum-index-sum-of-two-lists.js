9cc72da4e7b5a343e4a3bf3fdb30f155b9f5bdc3
Vy Tran <vtran1022@gmail.com>
2021-08-13 16:04:06 -0700

    Time: 128 ms (29.45%), Space: 46.9 MB (47.57%) - LeetHub

---
+++ minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    let commonShop = [];
+    let shopTracker = {};
+    list1.forEach((restaurant, i) => {
+        let j = list2.indexOf(restaurant)
+        if (j !== -1) {
+            shopTracker[restaurant] = i + j;
+        }
+    })
+    
+    let min = Math.min(...Object.values(shopTracker));
+    for (let [key, value] of Object.entries(shopTracker)) {
+        if (value === min) {
+            commonShop.push(key);
+        }
+    }
+    return commonShop;
+};
+
+/*
+I: two lists - arrays of restaurants
+O: An array w/ common interest with the least list index sum
+C: 
+- if there is a tie, output all in no order requirement
+- There's always an answer
+E: none
+
+Iterate over both lists
+when there's a common element, add that to the tracker along with the sum of the indexes
+return the key where the sum is the lowest or all of them if there's a tie
+*/
